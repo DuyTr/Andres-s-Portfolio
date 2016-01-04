0fa5e6a85fbd1f6fe961016035626ec6fec878ba
Your Name <tranvuthanhduy@Duys-MacBook-Pro.local>
2016-01-03 20:25:47 -0800

    add link to footer buttons

--- wp-content/themes/andresportfolio/footer.php
+++ wp-content/themes/andresportfolio/footer.php
@@ -11,9 +11,9 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="contact-info">
-					<i class="fa fa-linkedin-square"></i>
-					<i class="fa fa-instagram"></i>
-					<i class="fa fa-envelope"></i>
+					<a href="https://www.linkedin.com/in/andres-navas-eit-20b20b36" target="_blank"><i class="fa fa-linkedin-square"></i></a>
+					<a href="https://www.instagram.com/an_navas/" target="_blank"><i class="fa fa-instagram"></i></a>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>contact"><i class="fa fa-envelope"></i><a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
